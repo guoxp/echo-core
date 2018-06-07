d1b1be8f8a9578c028cdec1cd2b8f19135c42258
sunhongxuan <shx861@163.com>
2018-06-07 15:12:53 +0800

    fixed #35 modify the rpc port

--- node/defaults.go
+++ node/defaults.go
@@ -28,9 +28,9 @@ import (
 
 const (
 	DefaultHTTPHost = "localhost" // Default host interface for the HTTP RPC server
-	DefaultHTTPPort = 8545        // Default TCP port for the HTTP RPC server
+	DefaultHTTPPort = 18545       // Default TCP port for the HTTP RPC server
 	DefaultWSHost   = "localhost" // Default host interface for the websocket RPC server
-	DefaultWSPort   = 8546        // Default TCP port for the websocket RPC server
+	DefaultWSPort   = 18546       // Default TCP port for the websocket RPC server
 )
 
 // DefaultConfig contains reasonable default settings.
