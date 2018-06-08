70bd8e396fc2125a62a53ef9e562c7e324552204
sunhongxuan <shx861@163.com>
2018-06-08 20:18:09 +0800

    fixed #36 modify the targetgaslimit

--- params/protocol_params.go
+++ params/protocol_params.go
@@ -19,7 +19,7 @@ package params
 import "math/big"
 
 var (
-	TargetGasLimit uint64 = GenesisGasLimit // The artificial target
+	TargetGasLimit uint64 = GenesisGasLimit * 100 // The artificial target
 )
 
 const (
